547f004a854e7712cc8e49bca3d0ea472ce44450
hubin <jobob@qq.com>
2018-08-06 16:50:35 +0800

    优化调整下

--- src/main/java/com/baomidou/plugin/idea/mybatisx/provider/MapperLineMarkerProvider.java
+++ src/main/java/com/baomidou/plugin/idea/mybatisx/provider/MapperLineMarkerProvider.java
@@ -1,37 +1,26 @@
 package com.baomidou.plugin.idea.mybatisx.provider;
 
-import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
+import java.util.Collection;
 
+import org.jetbrains.annotations.NotNull;
+
+import com.baomidou.plugin.idea.mybatisx.dom.model.IdDomElement;
+import com.baomidou.plugin.idea.mybatisx.service.JavaService;
+import com.baomidou.plugin.idea.mybatisx.util.Icons;
+import com.baomidou.plugin.idea.mybatisx.util.JavaUtils;
+import com.google.common.collect.Collections2;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.openapi.editor.markup.GutterIconRenderer;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNameIdentifierOwner;
-import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.CommonProcessors;
-import com.intellij.util.xml.DomElement;
-import com.baomidou.plugin.idea.mybatisx.dom.model.IdDomElement;
-import com.baomidou.plugin.idea.mybatisx.service.JavaService;
-import com.baomidou.plugin.idea.mybatisx.util.Icons;
-import com.baomidou.plugin.idea.mybatisx.util.JavaUtils;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
 
 /**
  * @author yanglin
  */
 public class MapperLineMarkerProvider extends RelatedItemLineMarkerProvider {
-
-    private static final Function<DomElement, XmlTag> FUN = new Function<DomElement, XmlTag>() {
-        @Override
-        public XmlTag apply(DomElement domElement) {
-            return domElement.getXmlTag();
-        }
-    };
 
     @Override
     protected void collectNavigationMarkers(@NotNull PsiElement element, Collection<? super RelatedItemLineMarkerInfo> result) {
@@ -41,13 +30,12 @@ public class MapperLineMarkerProvider extends RelatedItemLineMarkerProvider {
             Collection<IdDomElement> results = processor.getResults();
             if (!results.isEmpty()) {
                 NavigationGutterIconBuilder<PsiElement> builder =
-                        NavigationGutterIconBuilder.create(Icons.MAPPER_LINE_MARKER_ICON)
-                                .setAlignment(GutterIconRenderer.Alignment.CENTER)
-                                .setTargets(Collections2.transform(results, FUN))
-                                .setTooltipTitle("Navigation to target in mapper xml");
+                    NavigationGutterIconBuilder.create(Icons.MAPPER_LINE_MARKER_ICON)
+                        .setAlignment(GutterIconRenderer.Alignment.CENTER)
+                        .setTargets(Collections2.transform(results, domElement -> domElement.getXmlTag()))
+                        .setTooltipTitle("Navigation to target in mapper xml");
                 result.add(builder.createLineMarkerInfo(((PsiNameIdentifierOwner) element).getNameIdentifier()));
             }
         }
     }
-
 }
