e9bc968b24192f0ab9e1c955b06b20db728b51de
ls9527 <364173778@qq.com>
2020-10-15 16:28:35 +0800

    fixed format for the yml

--- src/main/java/com/baomidou/plugin/idea/mybatisx/alias/SpringBootPackageResolver.java
+++ src/main/java/com/baomidou/plugin/idea/mybatisx/alias/SpringBootPackageResolver.java
@@ -13,6 +13,7 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.parser.ParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -76,6 +77,8 @@ public class SpringBootPackageResolver extends PackageAliasResolver {
             }
 
 
+        } catch (ParserException e) {
+            logger.info("yml parse fail", e);
         } catch (IOException e) {
             logger.error("read alias exception", e);
         }
