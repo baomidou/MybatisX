5177437e8763fe902cac8725069c78c2290f4371
ls9527 <364173778@qq.com>
2020-11-27 23:48:51 +0800

    支持mp3,mp2的别名通配符

--- src/main/java/com/baomidou/plugin/idea/mybatisx/alias/PackageAliasResolver.java
+++ src/main/java/com/baomidou/plugin/idea/mybatisx/alias/PackageAliasResolver.java
@@ -1,5 +1,6 @@
 package com.baomidou.plugin.idea.mybatisx.alias;
 
+import com.baomidou.plugin.idea.mybatisx.util.SpringStringUtils;
 import com.baomidou.plugin.idea.mybatisx.util.StringUtils;
 import com.google.common.collect.Sets;
 import com.intellij.openapi.project.Project;
@@ -7,6 +8,8 @@ import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiPackage;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.spring.model.utils.AntPathMatcher;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,6 +23,8 @@ import java.util.Set;
  */
 public abstract class PackageAliasResolver extends AliasResolver {
 
+    public static final String MP3_FACTORY = "com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean";
+    public static final String MP2_FACTORY = " com.baomidou.mybatisplus.spring.MybatisSqlSessionFactoryBean";
     private JavaPsiFacade javaPsiFacade;
 
     /**
@@ -32,23 +37,89 @@ public abstract class PackageAliasResolver extends AliasResolver {
         this.javaPsiFacade = JavaPsiFacade.getInstance(project);
     }
 
+
     @NotNull
     @Override
     public Set<AliasDesc> getClassAliasDescriptions(@Nullable PsiElement element) {
+        boolean enableMybatisPlus = isEnableMybaitsPlus();
         Set<AliasDesc> result = Sets.newHashSet();
         for (String pkgName : getPackages(element)) {
             if (null == pkgName) {
                 continue;
             }
-            PsiPackage pkg = javaPsiFacade.findPackage(pkgName);
-            if (null != pkg) {
-                addAliasDesc(result, pkg);
-                for (PsiPackage tmp : pkg.getSubPackages()) {
-                    addAliasDesc(result, tmp);
-                }
-            }
+            addPackages(enableMybatisPlus, result, pkgName);
         }
         return result;
+    }
+
+    private void addPackages(boolean enableMybatisPlus, Set<AliasDesc> result, String pkgName) {
+        if (enableMybatisPlus) {
+            // mybatis-plus 通配符支持
+            if (pkgName.contains("*") &&
+                !pkgName.contains(",")
+                && !pkgName.contains(";")) {
+
+                int firstWildcard = pkgName.indexOf(".*");
+                String firstPackage = pkgName.substring(0, firstWildcard);
+                JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(project);
+                PsiPackage basePackage = javaPsiFacade.findPackage(firstPackage);
+                if (basePackage != null) {
+                    matchPackages(pkgName, basePackage, result);
+                }
+            }
+
+            String[] packages = SpringStringUtils.tokenizeToStringArray(pkgName, ",;");
+            for (String string : packages) {
+                addPackage(result, string);
+            }
+
+        } else {
+            // 默认mybatis注册包
+            addPackage(result, pkgName);
+        }
+    }
+
+    private Boolean isEnableMybaitsPlus() {
+        Boolean enable = null;
+        JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(project);
+        GlobalSearchScope scope = GlobalSearchScope.allScope(project);
+        PsiClass mp3Class = javaPsiFacade.findClass(MP3_FACTORY, scope);
+        if (mp3Class != null) {
+            enable = true;
+        }
+
+        if (enable != null) {
+            PsiClass mp2Class = javaPsiFacade.findClass(MP2_FACTORY, scope);
+            if (mp2Class != null) {
+                enable = true;
+            }
+        }
+        if (enable == null) {
+            enable = false;
+        }
+        return enable;
+
+    }
+
+    private void matchPackages(String pkgName, PsiPackage basePackage, Set<AliasDesc> result) {
+        for (PsiPackage subPackage : basePackage.getSubPackages()) {
+            if (AntPathMatcher.match(pkgName, subPackage.getQualifiedName())) {
+                addPackage(result, subPackage.getQualifiedName());
+            } else {
+                matchPackages(pkgName, subPackage, result);
+            }
+        }
+    }
+
+
+    private void addPackage(Set<AliasDesc> result, String pkgName) {
+        PsiPackage pkg = javaPsiFacade.findPackage(pkgName);
+        if (null != pkg) {
+            addAliasDesc(result, pkg);
+            for (PsiPackage tmp : pkg.getSubPackages()) {
+                addAliasDesc(result, tmp);
+            }
+        }
     }
 
     private void addAliasDesc(Set<AliasDesc> result, PsiPackage pkg) {
