27f57a29736a79c82f886bff43e9e631258c749f
ls9527 <364173778@qq.com>
2020-12-31 15:25:11 +0800

    插件版本升级为1.4.5, 修复内部别名返回空值报错

--- src/main/java/com/baomidou/plugin/idea/mybatisx/alias/InnerAliasResolver.java
+++ src/main/java/com/baomidou/plugin/idea/mybatisx/alias/InnerAliasResolver.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -83,6 +84,9 @@ public class InnerAliasResolver extends AliasResolver {
                     this.innerAliasDescs = aliasDescSet;
                 }
             }
+        }
+        if (innerAliasDescs == null) {
+            return Collections.emptySet();
         }
         return innerAliasDescs;
     }
